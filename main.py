59d1588eabeaf4c5ba1834cd50b0be0c30ee841c
Karim Kidiss <ozerioss@gmail.com>
2019-01-14 17:26:15 +0100

    Barebones of the bot

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import discord
 
-print("temp")
+client = discord.Client() #establish connection to the client
+token = "Rnfg2vYBYS_EvDBKbqudR3BDJRoSpmja"
+@client.event
+async def on_ready():
+    print("The bot is ready !")
+    await client.change_presence(game = discord.Game(name = "Thomas is a bitch"))
+
+@client.event
+async def on_message(message):
+    #To avoid infinite loops where the bot talks to itself
+    if(message.author == client.user):
+        return
+    if(message.content == "**"):
+        await client.send_message(message.channel, "Sup homie")
+
+client.run(token)
+
+
---
+++ test.py
@@ -0,0 +1,4 @@
+import discord
+
+print("hallelujah")
+
